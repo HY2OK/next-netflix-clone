00507a82aa04bcc3f9ca4a954ecdd776488b6f6c
jun_ <gurwns232@naver.com>
2024-04-01 19:33:21 +0900

    feat: netflix clone setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className="m-5">
+      <Button>hello from shadcn/ui</Button>
+      <h1>hello</h1>
+    </div>
+  )
+}
